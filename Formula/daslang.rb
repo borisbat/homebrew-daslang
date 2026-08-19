c46678c140cea6bc4ef4e458d100f9f980319c5e
Boris Batkin <bbatkin@gmail.com>
2026-08-19 06:33:26 -0700

    daslang formula @ v0.6.4-RC2

--- Formula/daslang.rb
+++ Formula/daslang.rb
@@ -1,23 +1,23 @@
 class Daslang < Formula
   desc "High-performance statically-typed scripting language for games and real-time applications"
   homepage "https://daslang.io"
-  version "0.6.4-rc1"
+  version "0.6.4-rc2"
   license "BSD-3-Clause"
 
   on_macos do
     on_arm do
-      url "https://github.com/GaijinEntertainment/daScript/releases/download/v0.6.4-RC1/daslang-bundle-darwin26-arm64.zip"
-      sha256 "f5f8423daa0b3b51e3d13cd3a18e640455c38c9b98b44ed360da9bcb0af55cd3"
+      url "https://github.com/GaijinEntertainment/daScript/releases/download/v0.6.4-RC2/daslang-bundle-darwin26-arm64.zip"
+      sha256 "4c8810219f172b6e3adfc9f12628ee851a943c305d3f816cbb00d4781ecb986d"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/GaijinEntertainment/daScript/releases/download/v0.6.4-RC1/daslang-bundle-linux-x86_64.zip"
-      sha256 "824b95bcf514fc7e40b0f34936da0a2fa230877cfae6c7877eb2601d996e5e44"
+      url "https://github.com/GaijinEntertainment/daScript/releases/download/v0.6.4-RC2/daslang-bundle-linux-x86_64.zip"
+      sha256 "a93f8ebb0be14a725a19cf64e268688b9849261fded5bd6169729f43413c4c76"
     end
     on_arm do
-      url "https://github.com/GaijinEntertainment/daScript/releases/download/v0.6.4-RC1/daslang-bundle-linux-arm64.zip"
-      sha256 "24349c7ec240f75f7bd491f31b99cf7e3c17f84cfb4d63f60013e317dfe6067b"
+      url "https://github.com/GaijinEntertainment/daScript/releases/download/v0.6.4-RC2/daslang-bundle-linux-arm64.zip"
+      sha256 "ccdf30bf3cc808cc524cd9a94765f80b2e2e2b7e016f35d2ed0c6521fa4d92b0"
     end
   end
 
